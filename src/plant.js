e07a215aa0d601d367324146ec587cb201393bbb
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-13 17:30:31 -0700

    Add business and UI logic
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>

--- src/plant.js
+++ src/plant.js
@@ -1,66 +1,7 @@
-// export default function plant() {
-// }
-
-// hydrate(), feed(), giveLight()
-
-// const hydrate = (plant) => {
-//   return {
-//     ...plant,
-//     water: (plant.water || 0) + 1
-//   }
-// };
-
-// const feed = (plant) => {
-//   return {
-//     ...plant,
-//     soil: (plant.soil || 0) + 1
-//   }
-// };
-
-// const giveLight = (plant) => {
-//   return {
-//     ...plant,
-//     light: (plant.light || 0) + 1
-//   }
-// }
-
-let plant = { soil: 0, light: 0, water: 0 };
-// hydrate(plant);
-// feed(plant);
-// giveLight(plant);
-
-const changeState = (state, prop, value) => ({
-    ...state,
-    [prop]: (state[prop] || 0) + value
-  })
-
-const changeState = (prop) => {
-  return (value) => {
-    return (state) => ({
-      ...state,
-      [prop]: (state[prop] || 0) + value
-    })
-  }
-}
-
-
-const feed = changeState("soil")
-const hydrate = changeState("water")
-const giveLight = changeState("light")
-
-feed(5)(plant)
-
-const blueFood = changeState("soil")(5)
-const greenFood = changeState("soil")(10)
-const yuckyFood = changeState("soil")(-5)
-
-blueFood(plant)
-let tamagotchi = {}
-const newStateTamagotchi = changeState("food")(5)(tamagotchi)
-
+// This function stores our state.
 const storeState = () => {
   let currentState = {};
-  return (stateChangeFunction) => {
+  return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
@@ -69,27 +10,35 @@ const storeState = () => {
 
 const stateControl = storeState();
 
-const fedPlant = stateControl(blueFood);
-const plantFedAgain = stateControl(greenFood);
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
 
+// We create four functions using our function factory. We could easily create many more.
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
 
-// const changeState3 = (state) => {
-//   return (prop) => {
-//     return (value) => ({
-//       ...state,
-//       [prop]: (state[prop] || 0) + value
-//     })
-//   }
-// }
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
 
-// let tamagotchi = {}
-// const changeTamagotchi = changeState3(tamagotchi)
-// const feedTamagotchi = changeState3(tamagotchi)("food")
+$(document).ready(function() {
 
-// changeTamagotchi("food")(5)
-// feedTamagotchi(5)
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
 
-// const createTamagotchi = changeState3({})
-// const tamagotchiBob = createTamagotchi("food")(5)
-// const tamagotchiJill = createTamagotchi("sleep")(5)
-// const createPetRocks = changeState3({})
+// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
